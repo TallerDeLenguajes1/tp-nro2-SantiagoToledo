a5d61ac50d1d07bfb1b9204a405b7e24ec38f81f
SantiagoToledo <santi.nt2@gmail.com>
2019-03-21 19:38:17 -0300

    arregle pt1, cambio de variables

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -6,16 +6,30 @@
 
 int main()
 {
-	int 	f,c;
-	double mt[N][M];
+	int 	f,c,i=1;
+	double mt[M][N];
 
-	for(f = 0 ; f<N ; f++){
-		for (c = 0; c<M ; c++)
+
+
+	for(f = 0 ; f<M ; f++){
+		for (c = 0; c<N ; c++)
 		{
-			printf("%lf", mt[f][c]);
+			mt[f][c] = 1+i;
+			i++;
 		}
 		printf("\n");
 	}
+
+
+	for(f = 0 ; f<M ; f++){
+		for (c = 0; c<N ; c++)
+		{
+			printf(" %.1f ", mt[f][c]);
+		}
+		printf("\n");
+	}
+
+	
 
 	return 0;
 }
